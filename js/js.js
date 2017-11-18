c620728ee91424603eb71e51736195511f571c53
Sky Walker <ukhhit@gmail.com>
2017-11-18 16:47:39 +0200

    betta

--- js/js.js
+++ js/js.js
@@ -1,8 +1,60 @@
-$(document).ready(function(){
-$('td').click(function(){
-	//alert('hello');
-	
-	console.log(this);
-	$(this).find('img').toggle();
-});
+$(document).ready(function() {
+
+    var $table = $("table");
+
+    var counter = 0;
+
+    var images = [];
+
+
+
+
+
+
+
+    $('td').click(function() {
+
+        let img = [];
+        var column = this.cellIndex;
+        var sectionRowIndex = $(this).closest('tr')[0].sectionRowIndex;
+        var src = $(this).find('img')[1].attributes[1].value;
+
+        img.push(column);
+        img.push(sectionRowIndex);
+        img.push(src);
+
+        $(this).find('img').toggle();
+
+         
+		images[counter++] = img;
+
+
+        console.log(counter)
+        if (counter > 1) {
+          
+
+            if(images[0][2] == images[1][2]){
+
+            	$($table[0].rows[(images[0][1])].children[(images[0][0])]).off('click');
+            	$($table[0].rows[(images[1][1])].children[(images[1][0])]).off('click');
+
+            } else {
+            	$($table[0].rows[(images[0][1])].children[(images[0][0])]).find('img').toggle();
+            	$($table[0].rows[(images[1][1])].children[(images[1][0])]).find('img').toggle();
+            	
+            }
+
+            images=[];
+            counter = 0;
+        }
+
+        
+ 		
+
+		
+        
+        console.log(counter);
+        console.log(images);
+
+    });
 });
